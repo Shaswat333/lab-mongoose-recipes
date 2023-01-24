b95687a84be0d3cb1de962ce115930c24b7ea7ad
Shaswat333 <shaswatpachhera21@gmail.com>
2023-01-24 17:30:46 +0000

    done

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
@@ -7,38 +6,41 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
-//Method 1 : Using Async Await
-
-const manageRecipes = async () => {
-  try {
-    // Connection to the database "recipe-app"
-    const dbConnection = await mongoose.connect(MONGODB_URI);
-    console.log(`Connected to the database: "${dbConnection.connection.name}"`);
-
-    // Before adding any recipes to the database, let's remove all existing ones
-    await Recipe.deleteMany();
-
-    // Run your code here, after you have insured that the connection was made
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-manageRecipes();
-
-//Method 2: Using .then() method
-//If you want to use this method uncomment the code below:
-
-/* mongoose
+// Connection to the database "recipe-app"
+//Connection to the database "recipe-app"
+mongoose
   .connect(MONGODB_URI)
-  .then((x) => {
+  .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany();
+    return Recipe.deleteMany()
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.insertMany(data)
+    .then((createdData) => console.log(createdData.map(obj => obj.title)))
+
+
   })
-  .catch((error) => {
+
+  .then(() => {
+    // Run your code here, after you have insured that the connection was made
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new:true})
+
+  })
+
+  .then(() => {
+    // Run your code here, after you have insured that the connection was made
+    return Recipe.deleteOne({title: "Carrot Cake"})
+  })
+
+  .then(() => {
+
+    mongoose.connection.close()
+    .then(()=> console.log("conection closed"))
+
+
+  })
+  .catch(error => {
     console.error('Error connecting to the database', error);
-  }); */
+  });
